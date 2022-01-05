180fd15da7db8c5199aefcd18007ee0ffa0165f6
Walid Wazini <wazini25@gmail.com>
2022-01-05 09:42:25 +0800

    GET request to load data from db

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,40 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      const url =
+        'https://vue-http-demo-7f5e9-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json';
+      fetch(url)
+        .then((res) => {
+          // Check if the request is succesful or not
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          // console.log(data);
+          const getResults = [];
+          for (const id in data) {
+            getResults.push({
+              idKey: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = getResults;
+          console.log(`We get ${this.results[1]}.`)
+        });
+    },
   },
 };
 </script>
